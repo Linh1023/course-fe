6cc4ad34b96222018b9d5edc8d0dcd3b109e3d6d
Duongg <lengocduong003@gmail.com>
2025-05-20 09:47:59 +0700

    add logout

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,9 @@
 import AUTH_API from "./endpoints/auth";
+import REFRESH_TOKEN_API from "./endpoints/refresh_token";
 
 const API = {
   AUTH: AUTH_API,
+  REFRESH_TOKEN: REFRESH_TOKEN_API, 
 };
 
 export default API;
---
+++ src/api/endpoints/refresh_token.ts
@@ -0,0 +1,9 @@
+import { API_HOST } from "@/utils/env";
+
+
+const REFRESH_TOKEN_API = {
+  DELETE_REFRESH_TOKEN : `${API_HOST}/api/refresh-token/delete-refresh-token`,
+
+};
+
+export default REFRESH_TOKEN_API;
