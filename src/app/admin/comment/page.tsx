1a527a4b8a65109cb8d52700809af30c6488a343
quanglinh <116327425+Linh1023@users.noreply.github.com>
2025-05-31 09:08:33 +0700

    fix comment ui

--- src/app/admin/comment/page.tsx
+++ src/app/admin/comment/page.tsx
@@ -10,7 +10,7 @@ const CommentPage = async () => {
   );
   return (
     <div className="flex flex-col gap-4">
-      <div className="h-[100px] flex items-center justify-between gap-2">
+      <div className=" flex items-center justify-between gap-2">
         <h1 className="text-xl font-semibold">Quản Lý Bình Luận</h1>
       </div>
       <Suspense
