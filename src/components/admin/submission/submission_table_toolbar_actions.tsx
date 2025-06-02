d1dedb119f5a18d4e97934b9fdfe619da89919d9
Duongg <lengocduong003@gmail.com>
2025-06-02 11:54:53 +0700

    add filter table submission admin

---
+++ src/components/admin/submission/submission_table_toolbar_actions.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { type Table } from "@tanstack/react-table"
+
+import { Button } from "@/components/ui/button"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { DeleteSubmissionsDialog } from "./delete_submission_dialog"
+// import { DeleteCategoriesDialog } from "./delete-categories-dialog"
+// import { CreateCategoryDialog } from "./create-category-dialog"
+
+
+interface SubmissionTableToolbarActionsProps {
+  table: Table<SubmissionAdminResponse>
+  isOpen: boolean, // giá trị để biết để Dialog bật tắt
+  setIsOpen: (v: boolean) => void, // cái này để Dialog nó set bật tắt
+}
+
+// SubmissionTableToolbarActions cái này là button nằm ở phía bên trái của filter dùng để xóa và tạo mới
+export function SubmissionTableToolbarActions(props: SubmissionTableToolbarActionsProps) {
+  const { table, isOpen, setIsOpen } = props
+
+  return (
+    <div className="flex items-center gap-2">
+
+      {table.getFilteredSelectedRowModel().rows.length > 0 && (
+        <>
+          <DeleteSubmissionsDialog
+            submissions={table
+              .getFilteredSelectedRowModel()
+              .rows.map((row) => row.original)}
+            hanldeDeletedSuccess={() => table.toggleAllRowsSelected(false)}
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            showTrigger={true}
+          />
+        </>
+      )}
+      
+      {/* <CreateCategoryDialog /> */}
+
+
+      {/**
+       * Other actions can be added here.
+       * For example, import, view, etc.
+       */}
+    </div>
+  )
+}
